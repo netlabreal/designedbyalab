9cdd6fb60c490a485b68de5301896bb4251c2759
Vasa <vasa@example.com>
2019-09-09 15:44:13 +1000

    09-09-2019 1

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Container from '@material-ui/core/Container';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
@@ -9,58 +9,97 @@ import Button from '@material-ui/core/Button';
 
 
 import Header from "../header";
-
+import Message from "../message";
 import './login.css';
 
-const Login = () => {
-    return (
-        <div>
-            <Header display={false}/>
-            <Container component="main" maxWidth="xs">
-                <div className="paper">
-                    <Avatar><i className="fa fa-user"/></Avatar>
-                    <Typography component="h1" variant="h5">войти</Typography>
-                    <form className="form">
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="password"
-                            type="password"
-                            label="Password"
-                            name="password"
-                            autoComplete="current-password"
-                        />
 
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                        >
-                            Войти
-                        </Button>
+class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            active: false,
+            email: '',
+            pass: ''
+        }
+    }
 
-                    </form>
-                </div>
-            </Container>
+    handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        this.setState({active: false});
+    }
+
+    onClick = (event) => {
+        event.preventDefault();
+        if(this.state.email !== '' && this.state.pass !== ''){
+            this.setState({active: true, email: '', pass: ''})
+        }
+
+    };
+
+    onChange = (name) => (ev) => {
+        this.setState({[name]: ev.target.value});
+    }
+
+    render() {
+        return (
+            <div>
+                <Header display={false}/>
+                <Container component="main" maxWidth="xs">
+                    <div className="paper">
+                        <Avatar><i className="fa fa-user"/></Avatar>
+                        <Typography component="h1" variant="h5">войти</Typography>
+                        <form className="form" method="POST">
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                value={this.state.email}
+                                onChange={this.onChange('email')}
+                            />
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="password"
+                                type="password"
+                                label="Password"
+                                name="password"
+                                autoComplete="current-password"
+                                value={this.state.pass}
+                                onChange={this.onChange('pass')}
+                            />
+
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                onClick={this.onClick}
+                            >
+                                Войти
+                            </Button>
+
+                        </form>
+                    </div>
+
+                    <Message active={this.state.active} color="red"
+                             text='Ошибка! Непраивльный Email либо пароль!' close={this.handleClose}/>
+                </Container>
 
 
-        </div>
+            </div>
 
-    );
+        );
+    }
 }
 
 export default Login;
